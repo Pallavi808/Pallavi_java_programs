376e4729c38350cca32f1160ba6468136d446d68
sujit <pallavip808@gmail.com>
2024-10-05 17:25:05 +0530

    add

---
+++ src/October/ex_04102024/Lab003.java
@@ -0,0 +1,5 @@
+package October.ex_04102024;
+
+public class Lab003 {
+    
+}
---
+++ src/October/ex_04102024/Lab005.java
@@ -0,0 +1,6 @@
+package October.ex_04102024;
+
+public class Lab005 {
+    public static void main(String[] args)
+    {}
+}
