df7c836cba47e01f4e24f94474541e8f83df9517
sujit <pallavip808@gmail.com>
2024-10-18 07:08:54 +0530

    task

--- src/October/ex_18102024/Lab69_switch.java
+++ src/October/ex_18102024/Lab69_switch.java
@@ -1,4 +1,46 @@
 package October.ex_18102024;
 
+import java.util.Scanner;
+
 public class Lab69_switch {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        int Number = Sc.nextInt();
+
+        switch(Number)
+        {
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Thursday");
+            break;
+            case 5:
+            System.out.println("Friday");
+            break;
+            case 6:
+            System.out.println("Saturday");
+            break;
+            case 7:
+            System.out.println("Sunday");
+            break;
+            default:
+            System.out.println("Enter valid number");
+            break;
+
+
+        }
+
+
+  System.out.println("Out of the loop");
+        Sc.close();
+    }
 }
